cc06af42270e7c6cd29e76cb2a4ea45590969a9a
YuriGL-1 <146495029+YuriGL-1@users.noreply.github.com>
2025-11-16 14:18:03 -0300

    Update autoclick.py

--- autoclick.py
+++ autoclick.py
@@ -5,27 +5,27 @@ import time
 import threading
 import winsound
 
-# definir fariaveis de tempo e clicks/seg
+# did set variables of time and clicks/second  | definir variaveis de tempo e clicks/seg
 pyautogui.PAUSE = 0
 pyautogui.FAILSAFE = False
 
-click_per_seg = 1000 # definir quantidade de segundo por clicke
-click_duration = None # duração | none = infinito |
+click_per_seg = 1000 # set quantity of seconds for click | definir quantidade de segundo por clicke
+click_duration = None # duration | none = endless |
 
-# variaveis de controle 
+# variable of controll
 clicando = False
 parar = False 
 contador = 0
 start_time = 0
 
 def autoclick():
-    global clicando, parar, contador, start_time
-    delay = 1.0 / click_per_seg # intervalo entre click
+    global clicando, parar, contador, start_time # pull the variables from outside the def | puxar as variáveis ​​externas de fora
+    delay = 1.0 / click_per_seg # interval in clicker
     
-    contador = 0
+    contador = 0 # counter
     start_time = time.time()
     
-    print(f'autoclicker ativado. ({click_per_seg} CPS)')
+    print(f'auto clicker enable. ({click_per_seg} CPS)')
     winsound.Beep(1500, 80)
     
     while clicando and not parar:
@@ -37,9 +37,10 @@ def autoclick():
         time.sleep(delay)
    
     winsound.Beep(600, 200)
-    print(f"\rCliques: {contador:,} | CPS: {contador/max(time.time()-start_time, 0.1):,.0f}", end="")
+    print(f"\rClicks: {contador:,} | CPS: {contador/max(time.time()-start_time, 0.1):,.0f}", end="")
 ##
 
+# start the auto clicker
 def começar():
     global clicando
     clicando = not clicando
@@ -48,18 +49,18 @@ def começar():
         thread.start()
     
     else:
-        print('autoclicker pausado no momento')
+        print('auto clicker paused in moment')
 
 
-# ATALHOS
+# shortcut | ATALHOS
 keyboard.add_hotkey('ctrl+shift+space', começar)
-print("Pressione CTRL+SHIFT+R para ativar/desativar o autoclicker")
-print("Pressione ESC para sair do programa")
+print("press CTRL+SHIFT+R for enable/disable the auto clicker")
+print("press ESC for exit the program")
 
-# finalizar programa com 'esc'
+# end the program with 'esc' | finalizar programa com 'esc'
 keyboard.wait('esc')
 parar = True
-print('programa encerrado.')
+print('program closed.')
 
 
 
